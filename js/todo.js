268eb3c35f85af415d4bb2afe2d3a52232fcfa99
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-11-19 17:58:33 +0600

    Removed console.log

---
+++ js/todo.js
@@ -0,0 +1,41 @@
+const message = document.querySelector('.todo_list__field');
+const form = document.querySelector('.new_todo');
+const list = document.querySelector('.todo');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (message.value === '') {
+   message.placeholder = 'Вы ничего не ввели'
+  } else {
+    message.placeholder = '';
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    let newBtn = document.createElement('button');
+
+    li.classList.add('todo_item');
+    span.innerHTML = message.value;
+    span.classList.add('todo_item-title')
+    newBtn.classList.add('todo_item-btn');
+    newBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+
+    li.appendChild(span);
+    li.appendChild(newBtn);
+    list.appendChild(li);
+
+    li.addEventListener('click', function () {
+      if (this.classList.contains('checked')) {
+        this.classList.remove('checked')
+      } else {
+        this.classList.add('checked')
+      }
+    });
+    newBtn.addEventListener('click', function () {
+      list.removeChild(li)
+    })
+  }
+
+
+});
+
+
+
