b81b1e0cabea267d4025ae3f5df128998e45aaeb
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-11-21 23:55:30 +0600

    Finished

--- js/guessNumber.js
+++ js/guessNumber.js
@@ -5,7 +5,7 @@ const text = document.querySelector('[data-guessNumber-text]');
 
 class GuessNumber {
   constructor(inputValue, currentText) {
-    this.number = Math.floor(Math.random() * 100 + 1);
+    this.number = Math.floor(Math.random() * 100 + 1)
     this.attempts = 0;
     this.currentText = currentText;
   }
@@ -19,7 +19,7 @@ class GuessNumber {
       inputNumber.value = ''
     } else if (+inputNumber.value > 100) {
       this.currentText.style.color = 'red';
-      this.currentText.innerHTML = 'Число не должно быть не больше 100!';
+      this.currentText.innerHTML = 'Число не должно быть больше 100!';
       this.attempts++
     } else if (+inputNumber.value < this.number) {
       this.currentText.style.color = 'black';
@@ -30,9 +30,9 @@ class GuessNumber {
       this.currentText.innerHTML = `Неправильно! Загаданное число меньше ${inputNumber.value}`;
       this.attempts++;
     } else if (+inputNumber.value === this.number && this.attempts === 0){
-      text.innerHTML = `Вау! Отгадал с первой попытки! Молодец! <strong>Число ${this.number}</strong>. Количество попыток ${this.attempts}`
+      text.innerHTML = `Вау! Отгадал с первой попытки! Молодец! <strong>Число: ${this.number}</strong>. Количество попыток: ${this.attempts}`
     } else {
-      text.innerHTML = `Поздравляю! Ты угадал число! <strong>Число ${this.number}</strong>. Количество попыток ${this.attempts}`
+      text.innerHTML = `Поздравляю! Ты угадал число! <strong>Число: ${this.number}</strong>. Количество попыток: ${this.attempts}`
     }
   }
 }
